3d82255e7eb51512120b891108eb95f15c3eb927
henloboi <67458503+henloboi@users.noreply.github.com>
2021-03-08 01:31:31 +0000

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,3 +7,4 @@ cd /drone/src/work/
 repo init -u git://github.com/lineageos/android.git -b lineage-17.1 --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 
 repo sync -c -j96 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
+
